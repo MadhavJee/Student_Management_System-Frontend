e8f3955ab47ce7df71e7a4a0ae940a0a3b5fd6d4
MadhavJee <madhavjeearrah2000@gmail.com>
2026-03-14 14:33:44 +0530

    Update API URL to Render backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://student-management-system-backend-qig7.onrender.com/api';
 
 export const ROLES = {
   ADMIN: 'admin',
